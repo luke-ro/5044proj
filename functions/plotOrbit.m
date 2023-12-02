64ae0b6c1f2865c6470c7c1a16af4d03e7a5b64c
amandamarlow <77698771+amanda1797@users.noreply.github.com>
2023-12-01 20:41:17 -0700

    Calculated trajectory in A frame

--- functions/plotOrbit.m
+++ functions/plotOrbit.m
@@ -3,14 +3,14 @@ function [] = plotOrbit(r_V, figTitle)
 %   Detailed explanation goes here
 
 figure
-plot3(r_V(:,1), r_V(:,2), r_V(:,3), 'LineWidth', 1.5)
+plot3(r_V(1,:), r_V(2,:), r_V(3,:), 'LineWidth', 1.5)
 hold on
 scatter3(0,0,0, 'filled')
 axis equal
 title(figTitle)
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
+xlabel('X [km]')
+ylabel('Y [km]')
+zlabel('Z [km]')
 grid on
 hold off
 
