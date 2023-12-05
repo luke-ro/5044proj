4858c52e146fc15d21195da32a93cc92609ba423
amandamarlow <77698771+amanda1797@users.noreply.github.com>
2023-12-05 13:39:16 -0700

    fixed ylabel modifier

--- functions/plotStates.m
+++ functions/plotStates.m
@@ -9,38 +9,38 @@ subplot(6,1,1)
 sgtitle(title, 'Interpreter', 'latex')
 plot(t, X(1,:));
 hold on
-ylabel([modifier,'$r_{\hat{X}}$ [km]'], 'Interpreter', 'latex')
+ylabel(strcat(modifier,'$r_{\hat{X}}$ [km]'), 'Interpreter', 'latex')
 % hold off
 
 subplot(6,1,2)
 plot(t, X(2,:));
 hold on
-ylabel([modifier,'$r_{\hat{Y}}$ [km]'], 'Interpreter', 'latex')
+ylabel(strcat(modifier,'$r_{\hat{Y}}$ [km]'), 'Interpreter', 'latex')
 % hold off
 
 subplot(6,1,3)
 plot(t, X(3,:));
 hold on
-ylabel([modifier,'$r_{\hat{Z}}$ [km]'], 'Interpreter', 'latex')
+ylabel(strcat(modifier,'$r_{\hat{Z}}$ [km]'), 'Interpreter', 'latex')
 xlabel('time [hours]')
 % hold off
 
 subplot(6,1,4)
 plot(t, X(4,:));
 hold on
-ylabel([modifier,'$v_{\hat{X}}$ [km/s]'], 'Interpreter', 'latex')
+ylabel(strcat(modifier,'$v_{\hat{X}}$ [km/s]'), 'Interpreter', 'latex')
 % hold off
 
 subplot(6,1,5)
 plot(t, X(5,:));
 hold on
-ylabel([modifier,'$v_{\hat{Y}}$ [km/s]'], 'Interpreter', 'latex')
+ylabel(strcat(modifier,'$v_{\hat{Y}}$ [km/s]'), 'Interpreter', 'latex')
 % hold off
 
 subplot(6,1,6)
 plot(t, X(6,:));
 hold on
-ylabel([modifier,'$v_{\hat{Z}}$ [km/s]'], 'Interpreter', 'latex')
+ylabel(strcat(modifier,'$v_{\hat{Z}}$ [km/s]'), 'Interpreter', 'latex')
 xlabel('time [hours]')
 % hold off
 
