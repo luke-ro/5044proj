99b4185b15e42399272527f0370cf3714678728c
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-12-04 20:21:18 -0700

    added legend and allow for arbitrary landmarks

--- functions/plotMeasurements.m
+++ functions/plotMeasurements.m
@@ -2,21 +2,29 @@ function [] = plotMeasurements(t, us, vs, lmks_visible, lmksToPlot)
 %PLOTMEASUREMENTS Summary of this function goes here
 %   Detailed explanation goes here
 
+% shapes = ['o','+','*','.','x','square',"diamond",'v','^','>',"Pentagram","Hexagram", "Horizontal line", "Vertical line"];
 shapes = ['o','+','*','.','x','square',"diamond",'v','^','>'];
 
 figure
 hold on
-for lmk = lmksToPlot
+for i= 1:length(lmksToPlot)
+    lmk = lmksToPlot(i);
     u_plot = lmks_visible(lmk,:).*us(lmk,:);
     u_plot(u_plot==0) = nan;
-    scatter(t,u_plot,shapes(lmk))
+    scatter(t,u_plot,shapes(mod(i-1,length(shapes))+1));
 end
+legend(split(int2str(lmksToPlot)))
+
 figure
 hold on
-for lmk = lmksToPlot
+for i = 1:length(lmksToPlot)
+    lmk = lmksToPlot(i);
     v_plot = lmks_visible(lmk,:).*vs(lmk,:);
     v_plot(v_plot==0) = nan;
-    scatter(t,v_plot,shapes(lmk))
+    scatter(t,v_plot,shapes(mod(i-1,length(shapes))+1))
+    
 end
+
+legend(split(int2str(lmksToPlot)))
 end
 
