81aed1f8a2a9a9c174c85338600cf6827264c55e
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-12-05 13:15:36 -0700

    can call plotStates multiple times to plot multiples states on top of eachother

--- functions/plotStates.m
+++ functions/plotStates.m
@@ -4,45 +4,45 @@ function [] = plotStates(t,X,title)
 
 t = t/60^2;
 
-figure
+
 subplot(6,1,1)
 sgtitle(title, 'Interpreter', 'latex')
 plot(t, X(1,:));
 hold on
 ylabel('$r_{\hat{X}}$ [m]', 'Interpreter', 'latex')
-hold off
+% hold off
 
 subplot(6,1,2)
 plot(t, X(2,:));
 hold on
 ylabel('$r_{\hat{Y}}$ [m]', 'Interpreter', 'latex')
-hold off
+% hold off
 
 subplot(6,1,3)
 plot(t, X(3,:));
 hold on
 ylabel('$r_{\hat{Z}}$ [m]', 'Interpreter', 'latex')
 xlabel('time [s]')
-hold off
+% hold off
 
 subplot(6,1,4)
 plot(t, X(4,:));
 hold on
 ylabel('$v_{\hat{X}}$ [m/s]', 'Interpreter', 'latex')
-hold off
+% hold off
 
 subplot(6,1,5)
 plot(t, X(5,:));
 hold on
 ylabel('$v_{\hat{Y}}$ [m/s]', 'Interpreter', 'latex')
-hold off
+% hold off
 
 subplot(6,1,6)
 plot(t, X(6,:));
 hold on
 ylabel('$v_{\hat{Z}}$ [m/s]', 'Interpreter', 'latex')
 xlabel('time [hours]')
-hold off
+% hold off
 
 end
 
