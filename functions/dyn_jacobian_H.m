74d78284997d7841e6274d7a2405d59127e42222
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-12-09 19:36:02 -0700

    formatting

--- functions/dyn_jacobian_H.m
+++ functions/dyn_jacobian_H.m
@@ -33,15 +33,6 @@ function [J] = dyn_jacobian_H(x, l, R,const)
     J22 = const.f*(-dot((l-r),k)*j2+dot(l-r,j)*k2)/(dot(l-r,k))^2;
     J23 = const.f*(-dot((l-r),k)*j3+dot(l-r,j)*k3)/(dot(l-r,k))^2;
     
-%     J21 = (-dot((l-r),k)*i2+dot(l-r,i)*k2)/(dot(l-r,k))^2;
-%     J12 = ((l1 - r1)*(i1*k2-i2*k1) + (l3 - r3)*(i3*k2 - i2*k3))/(dot(l-r,k))^2;
-%     J22 = ((l1 - r1)*(j1*k2-j2*k1) + (l3 - r3)*(j3*k2 - j2*k3))/(dot(l-r,j))^2;
-%     J13 = ((l2 - r2)*(i2*k3-i1*k3) + (l1 - r1)*(i1*k3 - i3*k1))/(dot(l-r,k))^2;
-%     J23 = ((l2 - r2)*(j2*k3-j1*k3) + (l1 - r1)*(j1*k3 - j3*k1))/(dot(l-r,j))^2;
-    
     J = [J11 J12 J13 0 0 0;
          J21 J22 J23 0 0 0];
-
-
-
 end
